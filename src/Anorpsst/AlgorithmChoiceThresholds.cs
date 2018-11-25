660dfbebb6fd5dcd5dfe5c0a6155773944735de9
Eamon Nerbonne <eamon@nerbonne.org>
2018-11-25 20:40:43 +0100

    Defaults might as well be computed, not stored, since it's stored non-locally anyhow

--- src/Anorpsst/AlgorithmChoiceThresholds.cs
+++ src/Anorpsst/AlgorithmChoiceThresholds.cs
@@ -9,32 +9,32 @@ namespace Anoprsst
         public int BottomUpInsertionSortBatchSize;
         public int QuickSortFastMedianThreshold;
         public int MinimalParallelQuickSortBatchSize;
-        public static readonly AlgorithmChoiceThresholds<T> Defaults = PickDefaults();
 
-        static AlgorithmChoiceThresholds<T> PickDefaults()
-        {
-            if (!typeof(T).IsValueType) {
-                return new AlgorithmChoiceThresholds<T> {
-                    TopDownInsertionSortBatchSize = 24,
-                    BottomUpInsertionSortBatchSize = 16,
-                    QuickSortFastMedianThreshold = 10_000,
-                    MinimalParallelQuickSortBatchSize = 1500,
-                };
-            } else if (Unsafe.SizeOf<T>() <= 8) {
-                return new AlgorithmChoiceThresholds<T> {
-                    TopDownInsertionSortBatchSize = 64,
-                    BottomUpInsertionSortBatchSize = 40,
-                    QuickSortFastMedianThreshold = 13_000,
-                    MinimalParallelQuickSortBatchSize = 1100,
-                };
-            } else {
-                var topDownInsertionSortBatchSize = Math.Max(8, 550 / Unsafe.SizeOf<T>());
-                return new AlgorithmChoiceThresholds<T> {
-                    TopDownInsertionSortBatchSize = topDownInsertionSortBatchSize,
-                    BottomUpInsertionSortBatchSize = topDownInsertionSortBatchSize * 2 / 3,
-                    QuickSortFastMedianThreshold = 16_000,
-                    MinimalParallelQuickSortBatchSize = 1000,
-                };
+        public static AlgorithmChoiceThresholds<T> Defaults {
+            get {
+                if (!typeof(T).IsValueType) {
+                    return new AlgorithmChoiceThresholds<T> {
+                        TopDownInsertionSortBatchSize = 24,
+                        BottomUpInsertionSortBatchSize = 16,
+                        QuickSortFastMedianThreshold = 10_000,
+                        MinimalParallelQuickSortBatchSize = 1500,
+                    };
+                } else if (Unsafe.SizeOf<T>() <= 8) {
+                    return new AlgorithmChoiceThresholds<T> {
+                        TopDownInsertionSortBatchSize = 64,
+                        BottomUpInsertionSortBatchSize = 40,
+                        QuickSortFastMedianThreshold = 13_000,
+                        MinimalParallelQuickSortBatchSize = 1100,
+                    };
+                } else {
+                    var topDownInsertionSortBatchSize = Math.Max(8, 550 / Unsafe.SizeOf<T>());
+                    return new AlgorithmChoiceThresholds<T> {
+                        TopDownInsertionSortBatchSize = topDownInsertionSortBatchSize,
+                        BottomUpInsertionSortBatchSize = topDownInsertionSortBatchSize * 2 / 3,
+                        QuickSortFastMedianThreshold = 16_000,
+                        MinimalParallelQuickSortBatchSize = 1000,
+                    };
+                }
             }
         }
     }
