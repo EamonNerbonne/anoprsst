d020fa636bcfe37bda5a6d1218d83c61c5a6e750
Eamon Nerbonne <eamon@nerbonne.org>
2019-01-12 23:05:22 +0100

    Remove pointless array overloads

--- src/Anoprsst/SpanExtensions.cs
+++ src/Anoprsst/SpanExtensions.cs
@@ -17,20 +17,6 @@ namespace Anoprsst
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Obsolete("This method has been renamed to "+nameof(WithOrder) + ".", true)]
-        public static SortableSpan<T, TOrder> SortUsing<T, TOrder>(this T[] block, TOrder ordering)
-            where TOrder : struct, IOrdering<T>
-            => new SortableSpan<T, TOrder>(block, ordering);
-
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Obsolete("This method has been renamed to "+nameof(WithIComparableOrder) + ".", true)]
-        public static SortableSpan<T, ComparableOrdering<T>> SortIComparableUsing<T>(this T[] block)
-            where T : struct, IComparable<T>
-            => new SortableSpan<T, ComparableOrdering<T>>(block, default);
-
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [Obsolete("This method has been renamed to "+nameof(WithIComparableOrder) + ".", true)]
         public static SortableSpan<T, ComparableOrdering<T>> SortIComparableUsing<T>(this Span<T> block)
             where T : struct, IComparable<T>
@@ -41,18 +27,6 @@ namespace Anoprsst
         public static SortableSpan<T, TOrder> WithOrder<T, TOrder>(this Span<T> block, TOrder ordering)
             where TOrder : struct, IOrdering<T>
             => new SortableSpan<T, TOrder>(block, ordering);
-
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static SortableSpan<T, TOrder> WithOrder<T, TOrder>(this T[] block, TOrder ordering)
-            where TOrder : struct, IOrdering<T>
-            => new SortableSpan<T, TOrder>(block, ordering);
-
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static SortableSpan<T, ComparableOrdering<T>> WithIComparableOrder<T>(this T[] block)
-            where T : struct, IComparable<T>
-            => new SortableSpan<T, ComparableOrdering<T>>(block, default);
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
